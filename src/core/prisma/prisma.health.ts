c4953bd48fad38804ebe9e356571f3d1dd8b0cfb
ultimatefaloe <ultimatefaloe@outlool.com>
2025-11-05 04:51:55 +0100

    template updated

--- src/core/prisma/prisma.health.ts
+++ src/core/prisma/prisma.health.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class PrismaHealthService extends HealthIndicator {
